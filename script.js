0ccf1311bded2de554d465f7a31e08e81c5559db
Smil <me@smilt.dev>
2025-09-16 21:58:17 +0300

    refactor: Continue work on reactive design, rewrite location switching system, rewrite start scene, add location divs, a lot more

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 
 // location System
 const locations = [
-    {"name": "daveLoc"},
-    {"name": "basementLoc"},
-    {"name": "crystalLoc"}
+    {"name": "daveLocation"},
+    {"name": "atticLocation"},
+    {"name": "basementALocation"},
+    {"name": "basementBLocation"},
+    {"name": "outsideALocation"},
+    {"name": "outsideBLocation"},
+    {"name": "crystalLocation"},
+    {"name": "hobbitLocation"}
 ]
-var currentLocation = "daveLoc"
+var currentLocation = "daveLocation"
 
 // dave Texts on start
 const initText = [
@@ -16,7 +21,21 @@ const initText = [
     {"t": "theres a bulletin board with some info", "to": 1000},
     {"t": "idk why its there, but its pretty cool ngl", "to": 3000},
     {"t": "wait you think i have more to say?", "to": 2800},
-    {"t": "you can click on me to talk if you want", "to": 1, click: true}
+    {"t": "you can click on me to talk if you want", "to": 100, click: true}
+]
+
+const initTextAfterExplsion = [
+    {"t": "didnt ask lmao!", "to": 500},
+    {"t": "come in my crib tho", "to": 1000},
+    {"t": "theres not much here", "to": 100, bg: true},
+    {"t": "wait i know you...", "to": 1000},
+    {"t": "YOU BROKE THE CRYSTAL!!", "to": 1000},
+    {"t": "i cant believe this", "to": 1000},
+    {"t": "because of you, the world reset", "to": 1000},
+    {"t": "good thing i backed up the scene yesterday", "to": 1000},
+    {"t": "but still...", "to": 1000},
+    {"t": "you owe me big time", "to": 3000},
+    {"t": "at this point, do i want to talk to you?", "to": 100, click: true}
 ]
 
 // allow clicking dave
@@ -25,17 +44,20 @@ var allowClickingDave = false
 // json object list with dave's responces
 var daveTextList = fetch("/assets/text.json").then(response => response.json())
 
-// parent of all elements below
-var daveLocation = document.getElementById("daveLoc") 
+// location elements
+var transitionLocation = document.getElementById("transitionLocation") 
+var daveLocation = document.getElementById("daveLocation") 
+var basementALocation = document.getElementById("basementALocation") 
+var basementBLocation = document.getElementById("basementBLocation") 
+var outsideALocation = document.getElementById("outsideALocation") 
+var outsideBLocation = document.getElementById("outsideBLocation") 
+var atticLocation = document.getElementById("atticLocation") 
+var crystalLocation = document.getElementById("crystalLocation") 
+var hobbitLocation = document.getElementById("hobbitLocation") 
+
+//// parent elements of daveLocation
 var daveText = document.getElementById("daveText") // Dave Text (above)
 var daveImage = document.getElementById("daveCharacter") // Dave Image (center)
-var daveTextBox = document.getElementById("daveTextBox") // Dave Text Box (below, used once)
-
-// location Buttons
-var btnDownstairs = document.getElementById("btnDownstairs")
-var btnUpstairs = document.getElementById("btnUpstairs")
-var btnToCrystal = document.getElementById("btnToCrystal")
-var btnFromCrystal = document.getElementById("btnFromCrystal")
 
 // Dave Text Animation
 // handling the text animation at the begining of the page
@@ -69,12 +91,10 @@ function changeLocation(to) {
     if (!locations.some(e => e.name === to)) return
     var loc = locations.find(e => e.name === to)
 
-    document.getElementById(currentLocation).style.visibility = "hidden"
-
-    // document.body.style.backgroundImage = `url('${loc.bg}')`
+    document.getElementById(currentLocation).classList.add("hidden")
 
     currentLocation = to
-    document.getElementById(currentLocation).style.visibility = "visible"
+    document.getElementById(currentLocation).classList.remove("hidden")
 }
 
 // On Load
@@ -87,26 +107,40 @@ window.addEventListener('DOMContentLoaded', () => {
         daveTextAnimation(text, textSpeed)
 
         setTimeout(() => {
-            daveTextBox.style.visibility = "visible"
-            daveTextBox.style.opacity = "1"
+            document.getElementById("daveTextBox").style.visibility = "visible"
+            document.getElementById("daveTextBox").style.opacity = "1"
         }, textSpeed * text.length + textSpeed)
     }, 3000)
+
+    // dotted text animation for transition text
+    setInterval(() => {
+        var dotText = document.getElementById("transitionDottedText")
+        
+        if (dotText.innerText.length >= 3) {
+            dotText.innerText = ""
+        } else {
+            dotText.innerText += "."
+        }
+    }, 200)
 })
 
 // On Enter Press in the Question Box
 // handle animations when submitted the question
-daveTextBox.addEventListener("keydown", function(event) {
+document.getElementById("daveTextBox").addEventListener("keydown", function(event) {
     const textSpeed = 75
 
     if (event.key === "Enter") {
-        if (!daveTextBox.value.endsWith("?")) {
+        if (!document.getElementById("daveTextBox").value.endsWith("?")) {
             daveTextAnimation("Thats not a question...", 120)
             return
         }
 
         event.preventDefault()
+        document.getElementById("daveTextBox").style.opacity = "0"
 
-        daveTextBox.style.opacity = "0"
+        if (localStorage.getItem("bigBang") === "true") {
+            initText = initTextAfterExplsion
+        }
 
         var delay = 0
         initText.forEach(e => {
@@ -114,8 +148,8 @@ daveTextBox.addEventListener("keydown", function(event) {
                 daveTextAnimation(e.t, textSpeed)
 
                 if (e.bg) { 
-                    daveLocation.style.backgroundColor = "transparent"
-                    document.getElementById("btnDownstairs").style.removeProperty("visibility")
+                    document.getElementById("daveBg").style.visibility = "visible"
+                    document.getElementById("daveBtnDownstairs").style.removeProperty("visibility")
                 }
                 if (e.click) { 
                     allowClickingDave = true 
@@ -125,7 +159,7 @@ daveTextBox.addEventListener("keydown", function(event) {
             delay += (textSpeed * e.t.length) + e.to
         })
         setTimeout(() => {
-            daveTextBox.remove()
+            document.getElementById("daveTextBox").remove()
         }, delay)
     }
 })
@@ -166,8 +200,27 @@ daveImage.addEventListener("click", function() {
     }
 })
 
-// On Button to Downstairs Click
-btnDownstairs.addEventListener("click", () => changeLocation("basementLoc"))
-btnUpstairs.addEventListener("click", () => changeLocation("daveLoc"))
-btnToCrystal.addEventListener("click", () => changeLocation("crystalLoc"))
-btnFromCrystal.addEventListener("click", () => changeLocation("basementLoc"))
+// On Button, change location (formating: btn[locationFrom]_[locationTo])
+// // from main
+// document.getElementById("btnDave_basementA").addEventListener("click", changeLocation("basementLocation"))
+// document.getElementById("btnDave_outsideA").addEventListener("click", changeLocation("outsideLocation"))
+// document.getElementById("btnDave_outsideB").addEventListener("click", changeLocation("outsideLocation"))
+// document.getElementById("btnDave_attic").addEventListener("click", changeLocation("atticLocation"))
+
+// // from basement A
+// document.getElementById("btnBasementA_dave").addEventListener("click", changeLocation("daveLocation"))
+// document.getElementById("btnBasementA_basementB").addEventListener("click", changeLocation("basementBLocation"))
+// document.getElementById("btnBasementA_crystal").addEventListener("click", changeLocation("crystalLocation"))
+// document.getElementById("btnBasementA_hobbit").addEventListener("click", changeLocation("hobbitLocation"))
+
+// // to dave
+// document.getElementById("btnBasementA_dave").addEventListener("click",changeLocation("daveLocation"))
+// document.getElementById("btnOutsideA_dave").addEventListener("click", changeLocation("daveLocation"))
+// document.getElementById("btnOutsideB_dave").addEventListener("click", changeLocation("daveLocation"))
+// document.getElementById("btnAttic_dave").addEventListener("click", changeLocation("daveLocation"))
+
+// // to basement A
+// document.getElementById("btnDave_basementA").addEventListener("click", changeLocation("basementLocation"))
+// document.getElementById("btnBasementB_basementA").addEventListener("click", changeLocation("basementLocation"))
+// document.getElementById("btnCrystal_basementA").addEventListener("click", changeLocation("basementLocation"))
+// document.getElementById("btnHobbit_basementA").addEventListener("click", changeLocation("basementLocation"))
